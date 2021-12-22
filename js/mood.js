2836f6c6fcf3c99ec9701a470f172d31f9fd9ff1
Bradley Wooten <gearfire@gmail.com>
2021-12-22 00:44:20 -0700

    x

--- js/mood.js
+++ js/mood.js
@@ -1,4 +1,4 @@
-var moods = [ 'hsla(60,100%,60%', 'hsla(190,100%,45%', 'hsla(348,100%,80%' ];
+var moods = [ 'hsla(60,100%,60%', 'hsla(190,100%,45%', 'hsla(348,100%,70%' ];
 var tone = moods [ Math.floor ( Math.random() * moods.length ) ];
 var mood = tone + ',1)';
 
