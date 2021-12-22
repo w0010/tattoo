e2bc513371177555b4676dfe51edb9faef3552db
Bradley Wooten <gearfire@gmail.com>
2021-12-21 23:29:13 -0700

    CRT added

--- js/mood.js
+++ js/mood.js
@@ -1,4 +1,4 @@
-var moods = [ 'hsla(57,85%,40%', 'hsla(170,100%,50%', 'hsla(240,100%,70%', 'hsla(345,100%,60%' ];
+var moods = [ 'hsla(57,85%,40%', 'hsla(170,100%,50%', 'hsla(345,100%,60%' ];
 var tone = moods [ Math.floor ( Math.random() * moods.length ) ];
 var mood = tone + ',0.9)';
 
