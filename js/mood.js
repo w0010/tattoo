fa5c1321d431f2c09414baedb40ce24463b3b9e1
Bradley Wooten <gearfire@gmail.com>
2021-12-22 00:00:11 -0700

    mood colors

--- js/mood.js
+++ js/mood.js
@@ -1,4 +1,4 @@
-var moods = [ 'hsla(57,85%,40%', 'hsla(170,100%,50%', 'hsla(345,100%,60%' ];
+var moods = [ 'hsla(60,100%,70%', 'hsla(190,100%,50%', 'hsla(353,100%,80%' ];
 var tone = moods [ Math.floor ( Math.random() * moods.length ) ];
 var mood = tone + ',0.9)';
 
