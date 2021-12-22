9a1980dcdeb1ccfe5dd97b331267efb38749d92a
Bradley Wooten <gearfire@gmail.com>
2021-12-22 12:14:38 -0700

    x

--- js/mood.js
+++ js/mood.js
@@ -7,5 +7,6 @@ $(document).ready(function() {
 document.documentElement.style.setProperty('--mood', mood);
 document.documentElement.style.setProperty('--fademood', tone + ',0.6)');
 document.documentElement.style.setProperty('--dimmood', tone + ',0.15)');
+document.documentElement.style.setProperty('--nomood', tone + ',0)');
 
 });
