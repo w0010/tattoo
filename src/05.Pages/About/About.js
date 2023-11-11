5bdd9ff787c870248d851df77da11ff2532e9c2a
w0010 <44535432+w0010@users.noreply.github.com>
2023-11-10 20:48:54 -0500

    removed Poem

--- src/05.Pages/About/About.js
+++ src/05.Pages/About/About.js
@@ -1,7 +1,6 @@
 import BradWooten from '../../03.Organisms/BradWooten/BradWooten';
 import Bio from '../../03.Organisms/Bio/Bio'; 
 import Assets from '../../03.Organisms/Assets/Assets';
-import Poem from '../../03.Organisms/Poem/Poem';
 
 import './About.css';
 
@@ -13,7 +12,6 @@ const About = () => {
             <BradWooten />
             <Bio data-number="BIO" />
             <Assets data-number="02" />
-            <Poem />
         </main>
     )
 }
